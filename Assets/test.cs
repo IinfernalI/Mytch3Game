03417d4ff6e1f5589020b08356956ef266e6cbd4
Arthur_Lebedev <IinfernalI21@gmail.com>
2022-07-26 10:12:25 +0300

    Add_scripts, Add_gameObjects

--- Assets/test.cs
+++ Assets/test.cs
@@ -1,23 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class test : MonoBehaviour
+
+
+
+public class test
 {
     // Start is called before the first frame update
+    private PP p = new PP();
+    
     
     [SerializeField]int ss = 0;
     void Start()
     {
+        int s = 10;
+        p.MessageTarget += Fire(1);
+        
         if (ss != 20)
         {
             Debug.Log($"grrrr");
         }
     }
+    
 
-    // Update is called once per frame
-    void Update()
+
+    public int Fire(int x)
     {
-        
+        x++;
+        Console.WriteLine("Fire");
+        return x;
     }
+}
+
+public delegate void ActSSion<in T>(T obj);
+class PP
+{
+    
+    public event Action<int,int> MessageTarget;
+
+    private delegate void Action<in T>(T obj);
+    
 }
