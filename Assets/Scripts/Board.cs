bca359b342d68fb7ee11a6a89fc23325429ec408
Arthur_Lebedev <IinfernalI21@gmail.com>
2022-07-28 18:40:47 +0300

    Add_method_rotate_icons&tile_pos

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,6 +1,8 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using DG.Tweening;
 using UnityEngine;
 
 public sealed class Board : MonoBehaviour
@@ -12,6 +14,8 @@ public sealed class Board : MonoBehaviour
     public Tile[,] Tiles { get; private set; }
 
     private readonly List<Tile> _selection = new List<Tile>();
+
+    private const float TweenDuration = 0.25f;
 
     public int Width => Tiles.GetLength(0);
     public int Height => Tiles.GetLength(1);
@@ -39,7 +43,7 @@ public sealed class Board : MonoBehaviour
         }
     }
 
-    public void Select(Tile tile)
+    public async void Select(Tile tile)
     {
         if (!_selection.Contains(tile)) { _selection.Add(tile); }
 
@@ -50,7 +54,38 @@ public sealed class Board : MonoBehaviour
         
         Debug.Log($"Selected tile at ({_selection[0].x}) , ({_selection[0].y}) and ({_selection[1].x}) , ({_selection[1].y})");
         
+        await Swap(_selection[0],_selection[1]);
+        
         _selection.Clear();
         
+    }
+
+    public async Task Swap(Tile tile1, Tile tile2)
+    {
+        var icon1 = tile1.icon;
+        var icon2 = tile2.icon;
+
+        var icon1Transform = icon1.transform;
+        var icon2Transform = icon2.transform;
+
+        var sequence = DOTween.Sequence();
+
+        sequence.Join(icon1Transform.DOMove(icon2Transform.position, TweenDuration))
+                .Join(icon2Transform.DOMove(icon1Transform.position, TweenDuration));
+
+
+        await sequence.Play()
+            .AsyncWaitForCompletion();
+        
+        icon1Transform.SetParent(tile2.transform);
+        icon2Transform.SetParent(tile1.transform);
+
+        tile1.icon = icon2;
+        tile2.icon = icon1;
+
+        var titleTempItem = tile1.Item;
+        tile1.Item = tile2.Item;
+        tile2.Item = titleTempItem;
+
     }
 }
